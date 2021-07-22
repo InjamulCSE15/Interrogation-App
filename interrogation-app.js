85757516592990496fed4f363a78752133592294
unknown <yeasin.mohammod018@gmail.com>
2021-07-23 01:36:30 +0600

    Questions Ready

---
+++ interrogation-app.js
@@ -0,0 +1,235 @@
+let questions =[
+    {
+        question: "Which of the following JavaScript cannot do?",
+        choices: ["JavaScript can react to events", "JavaScript can manipulate HTML elements",
+            "JavaScript can be use to validate data","All of the Above"],
+        correctAnswer: 3
+    },
+    {
+        question: "Whats so great about XML?",
+        choices: ["Easy data exchange", "High speed on network",
+            "Only B.is correct","Both A & B"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which is not a property of attribute Behaviour of <Marquee> tag?",
+        choices: ["alternate", "blur","scroll","slide"],
+        correctAnswer: 1
+    },
+    {
+        question: " _________ keyword is used to declare variables in javascript.",
+        choices: ["Var","Dim","String","None of the above"],
+        correctAnswer: 0
+    },
+    {
+        question: "The attribute used to define a new namespace is.",
+        choices: ["XMLNS", "XmlNameSpace","Xmlns","XmlNs"],
+        correctAnswer: 2
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+    {
+        question:"",
+        choices: [],
+        correctAnswer: 0
+    },
+
+]
