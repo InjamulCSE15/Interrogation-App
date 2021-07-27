255769b5a902673dd830c3890a13d47ae1eb5baf
unknown <yeasin.mohammod018@gmail.com>
2021-07-27 18:13:03 +0600

    bug fixed

--- interrogationApp.js
+++ interrogationApp.js
@@ -35,172 +35,175 @@ var questions =[
         question:"Which CSS property can provide to add an effect when changing from one style to another,without using Flash animations or javascript?",
         choices: ["Associations","Transitions","Transistor","None of the above"],
         correctAnswer: 1
-    },
+     },
     {
         question:"Which browser can supports the transition property?",
-        choices: ["Internet Explorer 10","Firefox","Chrome","All of the above"],
+         choices: ["Internet Explorer 10","Firefox","Chrome","All of the above"],
         correctAnswer: 3
-    },
+     },
     {
-        question:"____________ is the tainted property of a window object.",
-        choices: ["Pathname","Protocol","Defaultstatus","Host"],
+         question:"____________ is the tainted property of a window object.",
+       choices: ["Pathname","Protocol","Defaultstatus","Host"],
         correctAnswer: 2
-    },
+     },
     {
-        question:"In JavaScript, _________ is an object of the target language data type that encloses an object of the source language.",
+         question:"In JavaScript, _________ is an object of the target language data type that encloses an object of the source language.",
         choices: ["a wrapper","a link","a cursor","a form"],
-        correctAnswer: 0
-    },
-    {
-        question:"When a JavaScript object is sent to Java, the runtime engine creates a Java wrapper of type",
-        choices: ["ScriptObject","JSObject","JavaObject","Jobject"],
-        correctAnswer: 1
-    },
-    {
-        question:"What do you add to a template in order to control where page content goes?",
-        choices: ["Text Frames","HTML Controllers","Editable Regions","Page Content Controllers"],
-        correctAnswer: 2
-    },
-    {
-        question:"Which of the following is NOT a Style?",
-        choices: ["Linked","Embedded","Inline","Orthogonal"],
-        correctAnswer: 3
-    },
-    {
-        question:"Which of the following is not supported by older browsers?",
-        choices: ["CSS","Layers","Frames","All of the above"],
-        correctAnswer: 3
-    },
-    {
-        question:"Which type of style should you use if you want to use the formats on multiple pages?",
-        choices: ["Linked","Embedded","Inline","Orthogonal"],
-        correctAnswer: 0
-    },
-    {
-        question:"Orkut.com is owned by ___________",
-        choices: ["Google","Yahoo","Microsoft","Apple"],
-        correctAnswer: 0
-    },
-    {
-        question:"What would be the colours of the RGB where the hexadecimal values are #FF0000, #00FF00 and #0000FF respectively?",
-        choices: ["Blue, Green, Red", "Green, Blue, Red", "Red, Blue, Green","Red, Green, Blue"],
-        correctAnswer: 3
-    },
-    {
-        question:"Which of the following statement is not true regarding JavaScript?",
-        choices: ["JavaScript is an object-based language","JavaScript is event driven","JavaScript is a loosely typed language","A JavaScript embedded in an HTML document is compiled and executed by the client browser"],
-        correctAnswer: 3
-    },
-    {
-        question:" While working on a JavaScript project, in your JavaScript application, which function would you use to send messages to users requesting for text input?",
-        choices: ["Display()", "Prompt()","Alert()","Confirm()"],
-        correctAnswer: 1
-    },
-    {
-        question:"Which property does one use to align text to the right side of a block-level element in Cascading Style Sheets?",
-        choices: ["horizontal-align","align","block-align","text-align"],
-        correctAnswer: 3
-    },
-    {
-        question:"A ________ object is a reference to one of the classes in a Java package, such as netscape.javascript .",
-        choices: ["JavaArray","JavaClass","JavaObject","JavaPackage"],
-        correctAnswer: 1
-    },
-    {
-        question:"The JavaScript exception is available to the Java code as an instance of __________",
-        choices: ["netscape.javascript.JSObject","netscape.javascript.JSException","netscape.plugin.JSException","None of the above"],
-        correctAnswer: 1
-    },
-    {
-        question:"Which of the following statements is false about event handlers in JavaScript?",
-        choices: ["They can be included with input tags","They can be associated with end of file processing for a database application","They can be included with the form tag","They are generally used to call functions when triggered"],
-        correctAnswer: 1
-    },
-    {
-        question:"The trick to getting a ball to bounce around a Web page is to:",
-        choices: ["Add AnimationBounce layers","Add keyframes to the timeline","Add Bounce parameters to the Object properties","Dreamweaver does not support animation... use Flash instead"],
-        correctAnswer: 1
-    },
-    {
-        question:"By default, what's the Fps shown on the timeline?",
-        choices: ["15","1","20","huh?"],
-        correctAnswer: 0
-    },
-    {
-        question:"Which of the following is false?",
-        choices: ["The Site Map can be saved as an image","You can FTP files using Dreamweaver","You can create forms in Dreamweaver","None of the above"],
-        correctAnswer: 3
-    },
-    {
-        question:"Dreamweaver users work in the Document Window using one of how many views?",
-        choices: ["3","5","2","8"],
-        correctAnswer: 0
-    },
-    {
-        question:"The general definition of a(n) ____ is a set of linked documents with shared attributes, such as related topics, a similar design, or a shared purpose.",
-        choices: ["index","website","Internet","Homepage"],
-        correctAnswer: 1
-    },
-    {
-        question:"What type of technology allows you to verbally speak with someone over the Internet?",
-        choices: ["Wiki","Social network","Ephone","VoIP"],
-        correctAnswer: 3
-    },
-    {
-        question:"Dreamweaver's ____ feature allows users to select colors and make perfect color matches.",
-        choices: ["Color Cube","Palattes","HTML view","Eye dropper"],
-        correctAnswer: 3
-    },
-    {
-        question:"Which of the following is true about links by default?",
-        choices: ["An unvisited link is underlined and blue","A visited link is underlined and red","An active link is underlined and purple","All the above"],
-        correctAnswer: 0
-    },
-    {
-        question:"The href attribute in the link tag specifies the:",
-        choices: ["Destination of a link","Link","Hypertext","None of the above"],
-        correctAnswer: 0
-    },
-    {
-        question:"HTML Event Attributes onunload fires",
-        choices: ["When the browser window has been closed","When the browser window is minimized","When the browser window has changed focus to other window","After the page is finished loading"],
-        correctAnswer: 0
-    },
-    {
-        question:"HTML Form Events ___________, fires the moment that the element loses focus.",
-        choices: ["onblur","onfocus","onchange","onlostfocus"],
-        correctAnswer: 0
-    },
-    {
-        question:"Twitter limits the number of characters per tweet is ?",
-        choices: ["130","160","124","140"],
-        correctAnswer: 3
-    },
-    {
-        question:"What is cell padding?",
-        choices: ["Used to separate cell walls from their contents","Used to set space between cells","Both a and b above","Used to provide width to a cell"],
-        correctAnswer: 0
-    },
-    {
-        question:"What are meta tags used for?",
-        choices: ["To store information usually relevant to browsers and search engines","To only store information usually relevant to browsers","To only store information about search engines","To store information about external links"],
-        correctAnswer: 0
-    },
-    {
-        question:"Which file controls how your frames will appear?",
-        choices: ["Frameset","Master Document","Template","Timeline"],
-        correctAnswer: 0
-    },
-    {
-        question:"What can't layers do if you want to convert them to tables?",
-        choices: ["Be close","Contain a Color","Be larger than the target table","Overlap"],
-        correctAnswer: 2
-    },
-    {
-        question:"Which HTML attribute is used to define inline styles?",
-        choices: ["font","class","styles","style"],
-        correctAnswer: 2
-    },
+         correctAnswer: 0
+  }
+    //,
+    //  {
+    //      question:"When a JavaScript object is sent to Java, the runtime engine creates a Java wrapper of type",
+    //      choices: ["ScriptObject","JSObject","JavaObject","Jobject"],
+    //      correctAnswer: 1
+    //  }
+    //,
+    // {
+    //     question:"What do you add to a template in order to control where page content goes?",
+    //     choices: ["Text Frames","HTML Controllers","Editable Regions","Page Content Controllers"],
+    //     correctAnswer: 2
+    // },
+    // {
+    //     question:"Which of the following is NOT a Style?",
+    //     choices: ["Linked","Embedded","Inline","Orthogonal"],
+    //     correctAnswer: 3
+    // },
+    // {
+    //     question:"Which of the following is not supported by older browsers?",
+    //     choices: ["CSS","Layers","Frames","All of the above"],
+    //     correctAnswer: 3
+    // },
+    // {
+    //     question:"Which type of style should you use if you want to use the formats on multiple pages?",
+    //     choices: ["Linked","Embedded","Inline","Orthogonal"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"Orkut.com is owned by ___________",
+    //     choices: ["Google","Yahoo","Microsoft","Apple"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"What would be the colours of the RGB where the hexadecimal values are #FF0000, #00FF00 and #0000FF respectively?",
+    //     choices: ["Blue, Green, Red", "Green, Blue, Red", "Red, Blue, Green","Red, Green, Blue"],
+    //     correctAnswer: 3
+    // },
+    // {
+    //     question:"Which of the following statement is not true regarding JavaScript?",
+    //     choices: ["JavaScript is an object-based language","JavaScript is event driven","JavaScript is a loosely typed language","A JavaScript embedded in an HTML document is compiled and executed by the client browser"],
+    //     correctAnswer: 3
+    // },
+    // {
+    //     question:" While working on a JavaScript project, in your JavaScript application, which function would you use to send messages to users requesting for text input?",
+    //     choices: ["Display()", "Prompt()","Alert()","Confirm()"],
+    //     correctAnswer: 1
+    // },
+    // {
+    //     question:"Which property does one use to align text to the right side of a block-level element in Cascading Style Sheets?",
+    //     choices: ["horizontal-align","align","block-align","text-align"],
+    //     correctAnswer: 3
+    // },
+    // {
+    //     question:"A ________ object is a reference to one of the classes in a Java package, such as netscape.javascript .",
+    //     choices: ["JavaArray","JavaClass","JavaObject","JavaPackage"],
+    //     correctAnswer: 1
+    // },
+    // {
+    //     question:"The JavaScript exception is available to the Java code as an instance of __________",
+    //     choices: ["netscape.javascript.JSObject","netscape.javascript.JSException","netscape.plugin.JSException","None of the above"],
+    //     correctAnswer: 1
+    // },
+    // {
+    //     question:"Which of the following statements is false about event handlers in JavaScript?",
+    //     choices: ["They can be included with input tags","They can be associated with end of file processing for a database application","They can be included with the form tag","They are generally used to call functions when triggered"],
+    //     correctAnswer: 1
+    // },
+    // {
+    //     question:"The trick to getting a ball to bounce around a Web page is to:",
+    //     choices: ["Add AnimationBounce layers","Add keyframes to the timeline","Add Bounce parameters to the Object properties","Dreamweaver does not support animation... use Flash instead"],
+    //     correctAnswer: 1
+    // },
+    // {
+    //     question:"By default, what's the Fps shown on the timeline?",
+    //     choices: ["15","1","20","huh?"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"Which of the following is false?",
+    //     choices: ["The Site Map can be saved as an image","You can FTP files using Dreamweaver","You can create forms in Dreamweaver","None of the above"],
+    //     correctAnswer: 3
+    // },
+    // {
+    //     question:"Dreamweaver users work in the Document Window using one of how many views?",
+    //     choices: ["3","5","2","8"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"The general definition of a(n) ____ is a set of linked documents with shared attributes, such as related topics, a similar design, or a shared purpose.",
+    //     choices: ["index","website","Internet","Homepage"],
+    //     correctAnswer: 1
+    // },
+    // {
+    //     question:"What type of technology allows you to verbally speak with someone over the Internet?",
+    //     choices: ["Wiki","Social network","Ephone","VoIP"],
+    //     correctAnswer: 3
+    // },
+    // {
+    //     question:"Dreamweaver's ____ feature allows users to select colors and make perfect color matches.",
+    //     choices: ["Color Cube","Palattes","HTML view","Eye dropper"],
+    //     correctAnswer: 3
+    //  }
+    //,
+    // {
+    //     question:"Which of the following is true about links by default?",
+    //     choices: ["An unvisited link is underlined and blue","A visited link is underlined and red","An active link is underlined and purple","All the above"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"The href attribute in the link tag specifies the:",
+    //     choices: ["Destination of a link","Link","Hypertext","None of the above"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"HTML Event Attributes onunload fires",
+    //     choices: ["When the browser window has been closed","When the browser window is minimized","When the browser window has changed focus to other window","After the page is finished loading"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"HTML Form Events ___________, fires the moment that the element loses focus.",
+    //     choices: ["onblur","onfocus","onchange","onlostfocus"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"Twitter limits the number of characters per tweet is ?",
+    //     choices: ["130","160","124","140"],
+    //     correctAnswer: 3
+    // },
+    // {
+    //     question:"What is cell padding?",
+    //     choices: ["Used to separate cell walls from their contents","Used to set space between cells","Both a and b above","Used to provide width to a cell"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"What are meta tags used for?",
+    //     choices: ["To store information usually relevant to browsers and search engines","To only store information usually relevant to browsers","To only store information about search engines","To store information about external links"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"Which file controls how your frames will appear?",
+    //     choices: ["Frameset","Master Document","Template","Timeline"],
+    //     correctAnswer: 0
+    // },
+    // {
+    //     question:"What can't layers do if you want to convert them to tables?",
+    //     choices: ["Be close","Contain a Color","Be larger than the target table","Overlap"],
+    //     correctAnswer: 2
+    // },
+    // {
+    //     question:"Which HTML attribute is used to define inline styles?",
+    //     choices: ["font","class","styles","style"],
+    //     correctAnswer: 2
+    // },
     // {
     //     question:"Which is the correct CSS syntax?",
     //     choices: ["body:color=black","{body;color:black}","{body:color=black(body}","body {color: black}"],
@@ -260,7 +263,7 @@ $(document).ready(function () {
                 }
                 else {
                     displayScore();
-                    $(document).find(".nextButton").text("Play Again");
+                    $(document).find(".nextButton").text("Play Again?");
                     interrogationOver = true;
                 }
             }
@@ -300,12 +303,12 @@ function displayCurrentQuestion() {
 
 function resetQuiz() {
     currentQuestion = 0;
-    correctAnswers = 0;
+    correctAnswer = 0;
     hideScore();
 }
 
 function displayScore() {
-    $(document).find(".interrogationContainer > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(document).find(".interrogationContainer > .result").text("You scored: " + correctAnswer + " out of: " + questions.length);
     $(document).find(".interrogationContainer > .result").show();
 }
 
